6926f395cd5f04f164bc37744394c66d3d59d494
canninjam <202.abhi@gmail.com>
2020-10-27 16:45:27 +0530

    Code checked in

---
+++ amazon-textract-code-samples-master/src-csharp/TextractExtensions/Form.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+
+namespace Amazon.Textract.Model
+{
+
+    public class Form
+    {
+        public List<Field> Fields { get; set; }
+        private Dictionary<string, Field> fieldMap;
+
+        public Form()
+        {
+            this.Fields = new List<Field>();
+            this.fieldMap = new Dictionary<string, Field>();
+        }
+
+        public void AddField(Field field)
+        {
+            this.Fields.Add(field);
+            try
+            {
+                this.fieldMap.Add(field.Key.ToString(), field);
+            }
+            catch 
+            { }
+        }
+        public Field GetFieldByKey(string key)
+        {
+            return this.fieldMap.GetValueOrDefault(key);
+        }
+
+        public List<Field> SearchFieldsByKey(string key)
+        {
+            return this.Fields.FindAll(f => f.Key.ToString().ToLower().Contains(key.ToLower()));
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", this.Fields);
+        }
+    }
+}
